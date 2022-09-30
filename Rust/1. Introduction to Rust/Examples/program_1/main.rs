77fc81139a7c18346a6b8e88edaf56243d13c20a
Tye Porter <tyeporterapps@gmail.com>
2022-09-29 21:30:38 -0500

    Add Rust Examples

---
+++ Rust/1. Introduction to Rust/Examples/program_1/main.rs
@@ -0,0 +1,8 @@
+/*
+    This is a simple program that prints "Hello, World!"
+    to standard output
+ */
+
+fn main() {
+    println!("Hello, World!");
+}
---
+++ Rust/1. Introduction to Rust/Examples/program_2/src/main.rs
@@ -0,0 +1,8 @@
+/*
+    This is a simple program that prints "Hello, Cargo!"
+    to standard output
+ */
+
+fn main() {
+    println!("Hello, Cargo!");
+}
